1e3a15db71634ca69dac60a0bbac69f8e7fa0c98
ishtiaq <khan98x@gmail.com>
2023-12-01 20:53:14 -0500

    add more test view

--- travel_itinerary/travel_itinerary_app/tests/test_views.py
+++ travel_itinerary/travel_itinerary_app/tests/test_views.py
@@ -1,32 +1,63 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
+from travel_itinerary_app.forms import SignupForm, LoginForm
 
-class ViewTestCase(TestCase): 
+class ViewTestCase(TestCase):
     def setUp(self):
-        self.signup_url = reverse('signup')
+        # Set up data for the tests
         self.user_data = {
-            'username': 'newuser',
-            'email': 'newuser@example.com',  # Assuming your form requires an email
-            'password1': 'testpassword123',
-            'password2': 'testpassword123',
-            # Include any other required fields from your SignupForm
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password': 'Testpassword123',
         }
-    
-    def test_signup_view_get(self):
+        self.user = User.objects.create_user(
+            username=self.user_data['username'], 
+            email=self.user_data['email'], 
+            password=self.user_data['password']
+        )
+
+        # URLs for views
+        self.home_url = reverse('home')
+        self.signup_url = reverse('signup')
+        self.login_url = reverse('login')
+        self.itinerary_url = reverse('itinerary')
+        self.profile_url = reverse('profile')
+        self.itinerary_form_url = reverse('itinerary_form')
+        self.search_results_url = reverse('search_results')
+
+    def test_home_view(self):
+        response = self.client.get(self.home_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'travel_itinerary_app/home.html')
+
+    def test_signup_view(self):
         response = self.client.get(self.signup_url)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'travel_itinerary_app/signup.html')
-    
-    def test_signup_view_post_success(self):
-        response = self.client.post(self.signup_url, self.user_data)
-        self.assertEqual(response.status_code, 302)  # Check for redirect after successful signup
-        self.assertRedirects(response, reverse('home'))  # Assuming 'home' is the name of your homepage URL
-    
-    def test_signup_view_post_failure(self):
-        # Modify user_data to create an invalid form submission, e.g., passwords don't match
-        invalid_user_data = self.user_data.copy()
-        invalid_user_data['password2'] = 'wrongpassword'
-        response = self.client.post(self.signup_url, invalid_user_data)
-        self.assertEqual(response.status_code, 200)  # Page reloads with form errors
-        # Use the actual error message from your form's validation
-        self.assertFormError(response, 'form', 'password2', ['The two password fields didn’t match.'])
+        self.assertIsInstance(response.context['form'], SignupForm)
+
+    def test_login_view(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.context['form'], LoginForm)
+
+    def test_itinerary_view(self):
+        self.client.login(username='testuser', password='Testpassword123')
+        response = self.client.get(self.itinerary_url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_view(self):
+        self.client.login(username='testuser', password='Testpassword123')
+        response = self.client.get(self.profile_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.user.username)
+
+
+    def test_search_results_view_post(self):
+        post_data = {
+            'business': 'test_business',
+            'Destination City': 'test_city'
+        }
+        response = self.client.post(self.search_results_url, post_data)
+        self.assertEqual(response.status_code, 200)
+ 
